49fb549e0afdb2e9e0ec7ba47729c47679533543
Duncan French <duncanlfrench@gmail.com>
2024-06-13 20:30:29 -0400

    Implemented functionality to give study sets titles.

--- free-flashcards-frontend/src/FreeFlashcards.js
+++ free-flashcards-frontend/src/FreeFlashcards.js
@@ -6,11 +6,15 @@ export default function FlashcardApp() {
   const [flashcardSets, updateFlashcardSets] = useState([]);
   const [nextId, setNextId] = useState(0);
 
-  function addFlashcardSet(addedSet) {
-    // will also need to add checks for empty flashcards
+  function addFlashcardSet(addedSet, setTitle) {
+    // will also need to add checks for empty flashcards and empty set
     if (addedSet.length !== 0) { // avoiding adding an empty set, this will also need to be checked on the server side since client side js can be inspect-elemented
-      updateFlashcardSets([...flashcardSets, {id: nextId, data: addedSet}]);
+      updateFlashcardSets([...flashcardSets, {id: nextId, title: setTitle, cards: addedSet}]);
       setNextId(nextId + 1);
+    }
+    console.log("Title: " + setTitle + ". Set: " + addedSet);
+    for (let i = 0; i < flashcardSets.length; i++) {
+      console.log(flashcardSets[i]);
     }
   }
   
--- free-flashcards-frontend/src/NewFlashcardSet.js
+++ free-flashcards-frontend/src/NewFlashcardSet.js
@@ -5,11 +5,12 @@ import { useState } from 'react';
 
 export default function NewFlashcardSet({addFlashcardSet}) {
     const [cards, updateCards] = useState([]);
-    const [nextId, updateNextId] = useState(0);
+    const [nextCardId, updateNextCardId] = useState(0);
+    const [setTitle, updateSetTitle] = useState("");
 
     function addCard() {
-        updateCards([...cards, {id: nextId, prompt: "", response: ""}]);
-        updateNextId(nextId + 1);
+        updateCards([...cards, {id: nextCardId, prompt: "", response: ""}]);
+        updateNextCardId(nextCardId + 1);
     }
     function removeCard(removedId) {
         updateCards(cards.filter(card => card.id !== removedId));
@@ -20,7 +21,12 @@ export default function NewFlashcardSet({addFlashcardSet}) {
         ));
     }
     function saveSet() {
-        addFlashcardSet(cards);
+        // add the set to the user's collection of sets - checks for invalid input should be done when attempting to add
+            // the ids in the cards on the frontend aren't relevant and should be different once the backend is called 
+            // (ensuring unique ids across all card sets is good)
+        addFlashcardSet(cards, setTitle);
+        updateCards([]); // clear the existing cards to make it simpler for the user to create another new set
+        updateSetTitle("");
     }
     // seems this is causing an error because each element doesn't have a unique id
     let cardList = cards.map(card => (
@@ -38,7 +44,10 @@ export default function NewFlashcardSet({addFlashcardSet}) {
         />
         <SaveNewSetButton
             save={saveSet}
-        />
+        /> <br/>
+        <label htmlFor="set-title">Set Title:</label>
+        <input type="text" name="set-title" id="set-title" value={setTitle} 
+            onChange={(e) => updateSetTitle(e.target.value)}></input>
         <ul className="new-card-list">
             {cardList}
         </ul>
