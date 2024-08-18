7b46e525496b3c80eb2b564de0b7ec8d51f93364
Duncan French <duncanlfrench@gmail.com>
2024-08-18 00:23:55 -0400

    Updated the StudySet controller to not require or consider a cards array in the request body to create a new set as the cards array should be an array of objectIDs.

--- free-flashcards-backend/Controllers/StudySet.Controller.js
+++ free-flashcards-backend/Controllers/StudySet.Controller.js
@@ -1,5 +1,11 @@
 const StudySet = require("../Models/StudySet.model");
 
+/*
+    This will likely need to interface with the FlashcardController and may even take over the 
+    responsibilities of the FlashcardRoute, as users will generally be interacting with the application 
+    via their sets and considering how they're modifying their sets
+*/
+
 const mongoose = require("mongoose");
 const express = require("express");
 const createError = require("http-errors");
@@ -9,13 +15,13 @@ module.exports = {
     createStudySet : async (request, response, next) => { // add a study set to the database
         console.log(request.body);
         try {
-            const set = new StudySet(request.body); // this works, assuming the input is valid
+            const set = new StudySet({"title": request.body.title, "cards": []}); // raises a 400 error if no title is included
             const result = await set.save();
             response.send(result);
         } catch (error) {
             console.log(error.message);
             if (error.name === "ValidationError") { // catching invalid input
-                next(createError(422, error.message))
+                next(createError(400, error.message)) // error code for BAD REQUEST
             }
             next(error);
         }
