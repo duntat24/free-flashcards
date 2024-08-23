4c54f7514d864428c942e03e33ac926eb0153273
Duncan French <duncanlfrench@gmail.com>
2024-08-22 20:57:59 -0400

    Added functionality to delete a study set.

--- free-flashcards-backend/Routes/StudySet.route.js
+++ free-flashcards-backend/Routes/StudySet.route.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const StudySetController = require("../Controllers/StudySet.Controller");
 
-// the route: root/sets
+// handles requests on the route <root>/sets
 
 router.post('/', StudySetController.createStudySet); // creates a new study set
+
+router.delete('/:id', StudySetController.deleteStudySet); // deletes the study set matching the provided id
 
 module.exports = router;
