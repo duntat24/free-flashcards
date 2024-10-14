27bfaf9bac397066c3f413ff76ee4ef8f0415a08
Duncan French <duncanlfrench@gmail.com>
2024-10-14 12:00:45 -0400

    Began implementing the page to edit a user study set, basing off functionality from the create study set page.

---
+++ free-flashcards-frontend/src/pages/StudySetEditor/EditableFlashcard.js
@@ -0,0 +1,104 @@
+import { useState } from 'react';
+
+export default function EditableFlashcard({card, removeCard, updateCard}) {
+    const MAX_FILE_SIZE = 500000; // defines maximum file size in bytes
+
+    // this function handles a change to the uploaded file when the user either adds a new file or removes their attached file
+    function handleUploadChange(event) { 
+        const changedFile = event.target.files[0];
+
+        const validateFileResult = validateFileInput(changedFile, MAX_FILE_SIZE);
+        if (validateFileResult === null) { // this means there is no file attached to the card
+            updateCard(card.prompt, card.response, card.id, {file: null, isPrompt: null}, card.userResponseType);
+        } else if (validateFileResult === "") { // validateFileInput returns the appropriate error message if a file input is invalid
+            updateCard(card.prompt, card.response, card.id, {file: changedFile, isPrompt: card.fileJSON.isPrompt}, card.userResponseType);
+        } else { // this means the validateFileResult method returned some sort of error message and we should respond appropriately
+            alert(validateFileResult);
+            event.target.value = null;
+            updateCard(card.prompt, card.response, card.id, {file: null, isPrompt: null}, card.userResponseType);
+        }
+    }
+    // this function handles a change with the radio button that defines whether an attached file should be displayed as part of a prompt or response
+    function handleFileAssociationChange(event) {
+        const isPrompt = event.target.value === "prompt"; // indicates whether the file will be associated with a prompt or a response
+        updateCard(card.prompt, card.response, card.id, {file: card.fileJSON.file, isPrompt: isPrompt}, card.userResponseType);
+    }
+    // this function handles a change with the dropdown that determines what type of response the user should give for the flashcard
+    function handleResponseTypeChange(event) {
+        updateCard(card.prompt, card.response, card.id, card.fileJSON, event.target.value);
+    }
+    // this function handles a change to the value of the 'prompt' field
+    function handlePromptChange(event) {
+        updateCard(event.target.value, card.response, card.id, card.fileJSON, card.userResponseType);
+    } 
+    // this function handles a change to the value of the 'response' field
+    function handleResponseChange(event) {
+        updateCard(card.prompt, event.target.value, card.id, card.fileJSON, card.userResponseType);
+    } 
+
+    // this function is responsible for creating radio button JSX, it allows dynamically determining which button should be checked
+    // this is important to have as functionality because one of the button should be initially checked if a file is present, but then which button is checked should be modifiable by a user
+    // the buttonType parameter indicates whether this is the prompt or response radio button
+    function generateRadioButtonJSX(buttonType) {
+        if (card.fileJSON === null) {
+            return null; // no need to generate this JSX if there is no file to associate with it
+        }
+        if (buttonType === "prompt" && card.fileJSON.partOfPrompt || buttonType === "response" && !card.fileJSON.partOfPrompt) {
+            return <input type="radio" id={`file-for-${buttonType}${card.id}`} name="file-association" value={buttonType}
+                onChange={handleFileAssociationChange} checked/>
+        }
+        return <input type="radio" id={`file-for-${buttonType}${card.id}`} name="file-association" value={buttonType}
+            onChange={handleFileAssociationChange}/>
+    }
+
+    // this JSX defines a radio button that allows a user to indicate whether an attached field should be displayed with the card's prompt or response
+    let fileAssociationJSX = <>
+        {generateRadioButtonJSX("prompt")}
+        <label htmlFor={"file-for-prompt" + (card.id)}>Prompt</label>
+        {generateRadioButtonJSX("response")}
+        <label htmlFor={"file-for-prompt" + (card.id)}>Response</label>
+        <br/>
+    </>
+
+    // this JSX is ultimately wrapped by <li> tags in an external method as react doesn't recognize keys properly if we return from inside this component
+    return <form> 
+        <label htmlFor={"prompt" + (card.id + 1)}>Prompt: </label> <input type="text" name="prompt" id={"prompt" + (card.id + 1)} value={card.prompt} 
+            onChange={handlePromptChange}/>
+        
+        <label htmlFor={"response" + (card.id + 1)}>Response: </label>
+        <input type="text" name="response" id={"response" + (card.id + 1)} value={card.response}
+            onChange={handleResponseChange}/>
+        
+        <label htmlFor={"response-type" + (card.id + 1)}>Response type: </label>
+        <select name="response-type" id={"response-type" + (card.id + 1)} onChange={handleResponseTypeChange}>
+            <option value="text">Text</option>
+            <option value="drawn">Drawn Image</option>
+            <option value="recorded">Record Audio</option>
+        </select>
+        
+        <input type="file" onChange={handleUploadChange}/>
+        {card.fileJSON === null ? <></> : fileAssociationJSX}
+        
+        <button className="delete-new-flaschard-button" onClick={() => removeCard(card.id)}>Del</button>
+    </form>
+}
+
+// this function verifies that uploaded files are within size and type constraints
+// if the file does not fit within those constraints, it returns an error message to be displayed
+// if it does, it returns a blank string
+function validateFileInput(file, MAX_FILE_SIZE) {
+    if (file === null || file === undefined) {
+        return null;
+    }
+    if (file.size > MAX_FILE_SIZE) {
+        return "Attached file is too large. Files must be less than 0.5 MB";
+    }
+    const fileMimetypeArray = file.type.split("/"); // separates keywords in the file's description, e.g. [image, jpeg]
+    if (fileMimetypeArray[0] !== "image" && fileMimetypeArray[0] !== "audio") {
+        return "Attached files must be image or audio files and cannot be PDFs";
+    }
+    if (fileMimetypeArray[1] === "tiff" || fileMimetypeArray[1] === "tiff-fx") { // TIFF files do not work in most browsers
+        return "Attached files cannot be in the following formats: TIFF";
+    }
+    return "";
+}
--- free-flashcards-frontend/src/pages/StudySetEditor/StudySetEditor.js
+++ free-flashcards-frontend/src/pages/StudySetEditor/StudySetEditor.js
@@ -1,3 +1,70 @@
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+
 export default function StudySetEditor({studySet, updateSet}) {
-    return <><h1>Editing</h1></>
+ 
+    const [modifiedSet, setModifiedSet] = useState([]);
+    const [cards, updateCards] = useState(studySet.cards);
+    const nextCardIdRef = useRef(1);
+
+    // this hook to fetches the cards in the targeted study set ONLY when the component mounts
+    useEffect(() => { 
+        const cardsUrl = "http://localhost:3001/cards/"; // just need to append a card's id to make this a get request
+        
+        if (studySet === null || studySet === undefined) { // if the targeted study set doesn't exist we shouldnt be trying to fetch its cards
+            return;
+        }
+        Promise.all(
+            studySet.cardIds.map((cardId) => axios.get(`${cardsUrl}${cardId}`)) 
+        ).then((data) => { // letting all the promises resolve before continuing
+            let addedCards = data.map((card) => { // creating an array containing all the fetched card data from the API
+                if (card.data.file !== undefined) { // if the card contains a file
+                    let cardFile = card.data.file;
+                    return {prompt: card.data.prompt, response: card.data.response, userResponseType: card.data.userResponseType,
+                            fileJSON: {data: arrayBufferToBase64(cardFile.data.data), 
+                                       fileType: cardFile.fileType, partOfPrompt: cardFile.partOfPrompt}};
+                } 
+                return {prompt: card.data.prompt, response: card.data.response, userResponseType: card.data.userResponseType}
+            });
+            setModifiedSet({...studySet, cards: addedCards}); // adding the fetched card data to the study set
+            console.log(addedCards);
+        }).catch((error) => {
+            console.log(error);
+        })        
+    // eslint-disable-next-line
+    }, []); // we only want to fetch & update the cards one time so we don't include a dependency array
+
+    // this adds a blank card to the newly created set
+    function addCard() { 
+        updateCards([...cards, {id: nextCardIdRef.current, prompt: "", response: "", fileJSON: {file: null, isPrompt: null}, userResponseType: "text"}]);
+        nextCardIdRef.current += 1;
+    }
+
+    // this removes the card with the specified id from the list of cards being added to the new stud yset
+    function removeCard(removedId) {
+        updateCards(cards.filter(card => card.id !== removedId)); // finding and removing the card with the specified id
+    }
+
+    // this updates a flashcard with the provided ID and sets its fields based on the passed parameters
+    function updateCard(newPrompt, newResponse, cardId, newFileJSON, newUserResponseType) { // this is used to update cards when the user edits a prompt or response
+        updateCards(cards.map(card => 
+            card.id === cardId ? {id: cardId, prompt: newPrompt, response: newResponse, 
+                                  fileJSON: newFileJSON, userResponseType: newUserResponseType}: card
+        ));
+    }
+    
+    return <><h1>Editing</h1>
+        <button>Save Changes</button>
+        <button>Discard Changes</button>
+    </>
+}
+
+// This method takes a file buffer from a request and converts it to a Base64 string to be displayed by our application
+function arrayBufferToBase64(buffer) {
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    for (let i = 0; i < bytes.byteLength; i++) { // by constructing our array iteratively we avoid errors from the call stack being too large
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
 }
